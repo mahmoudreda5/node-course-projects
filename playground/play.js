04cf83d64e4aae08cd85e6ccc42bcbc96da851af
mahmoud reda <r.engmahmoud7@gmail.com>
2021-07-19 05:11:20 +0200

    callback pattern in node

--- playground/play.js
+++ playground/play.js
@@ -1,17 +1,33 @@
+// // regular vs arrow functions
 // const sq = x => x * x;
 
 // console.log(sq(3));
 
-const event = {
-    name: 'past one',
-    guestlist: ['mahmoud', 'amina'],
-    list() {
-        console.log(this.name);
-        this.guestlist.forEach((guest) => {
-            console.log(guest + ' is attending ' + this.name);
-            console.log(this);
-        });
-    }
+// const event = {
+//     name: 'past one',
+//     guestlist: ['mahmoud', 'amina'],
+//     list() {
+//         console.log(this.name);
+//         this.guestlist.forEach((guest) => {
+//             console.log(guest + ' is attending ' + this.name);
+//             console.log(this);
+//         });
+//     }
+// };
+
+// event.list();
+
+// callback pattern
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            longitude: 0,
+            latitude: 0
+        };
+        callback(data);
+    }, 1000);
 };
 
-event.list();
+geocode('shoubra', (data) => {
+    console.log(data);
+});
