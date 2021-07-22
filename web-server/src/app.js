bbf83c18e12bef97ac4c13436bac870453c4cbb6
mahmoud reda <r.engmahmoud7@gmail.com>
2021-07-22 17:06:14 +0200

    add some html simple pages using express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
-    return res.send('hello express');
-});
-
-app.get('/help', (req, res) => {
-    return res.send('Help page');
-});
-
-app.get('/about', (req, res) => {
-    return res.send('about page');
-});
+const publicDirPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirPath));
 
 app.get('/weather', (req, res) => {
     return res.send('show weather');
