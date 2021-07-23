0a4debc22b211787802291a117c9a8a06c98027b
mahmoud reda <r.engmahmoud7@gmail.com>
2021-07-23 14:11:01 +0200

    use express static dir, assets and handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,8 +2,36 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// define paths for express config
 const publicDirPath = path.join(__dirname, '../public');
+const templatesDirPath = path.join(__dirname, 'templates');
+
+// setup handlebars config
+app.set('view engine', 'hbs');
+app.set('views', templatesDirPath);
+
+// setup static dir to serve
 app.use(express.static(publicDirPath));
+
+
+app.get('', (req, res) => {
+    return res.render('index', {
+        title: 'weather app',
+        name: 'mahmoud reda'
+    });
+});
+
+app.get('/about', (req, res) => {
+    return res.render('about', {
+        title: 'about me'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'help message'
+    });
+});
 
 app.get('/weather', (req, res) => {
     return res.send('show weather');
