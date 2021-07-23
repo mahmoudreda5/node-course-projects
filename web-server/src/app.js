ad993caa44a59f325fd00a719732ec42b1a3c7ad
mahmoud reda <r.engmahmoud7@gmail.com>
2021-07-23 14:34:14 +0200

    use hbs partials with express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
 // define paths for express config
 const publicDirPath = path.join(__dirname, '../public');
-const templatesDirPath = path.join(__dirname, 'templates');
+const templatesDirPath = path.join(__dirname, 'templates/views');
+const partialsDirPath = path.join(__dirname, 'templates/partials');
 
 // setup handlebars config
 app.set('view engine', 'hbs');
 app.set('views', templatesDirPath);
+hbs.registerPartials(partialsDirPath);
 
 // setup static dir to serve
 app.use(express.static(publicDirPath));
@@ -23,13 +27,15 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     return res.render('about', {
-        title: 'about me'
+        title: 'about me',
+        name: 'mahmoud reda'
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'help message'
+        title: 'help message',
+        name: 'mahmoud reda'
     });
 });
 
