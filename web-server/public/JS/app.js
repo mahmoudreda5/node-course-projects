b0c461664736c834ee65bf840d783796e52145da
mahmoud reda <r.engmahmoud7@gmail.com>
2021-07-24 03:54:28 +0200

    add some ui for weather fetching

--- web-server/public/JS/app.js
+++ web-server/public/JS/app.js
@@ -1,1 +1,22 @@
-console.log('js is loaded!');
+const weatherForm = document.querySelector('form');
+const query = document.querySelector('input');
+const messageOne = document.querySelector('#success');
+const messagetwo = document.querySelector('#error');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    messageOne.textContent = 'loading...';
+    const location = query.value;
+    fetch(`/weather?location=${location}`)
+    .then(response => {
+        response.json().then(data => {
+            if(data.error) {
+                messageOne.textContent = '';
+                messagetwo.textContent = data.error;
+            } else {
+                messagetwo.textContent = '';
+                messageOne.textContent = data.forecast;
+            }
+        });
+    });
+});
