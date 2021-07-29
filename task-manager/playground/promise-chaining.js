371fb4a531b50a31741b38f04a839f30267b70e8
mahmoud reda <r.engmahmoud7@gmail.com>
2021-07-29 09:49:09 +0200

    play with aync/await

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -11,11 +11,29 @@ const Task = require('../src/models/task');
 //     console.log(error);
 // });
 
-Task.findByIdAndDelete('61024a2ba197b608f4455fd0').then(task => {
-  console.log(task);
-  return Task.countDocuments({ completed: false }); 
-}).then(count => {
-    console.log(count);
-}).catch(error => {
-    console.log(error);
-})
+// const updateAgeAndCount = async (id, age) => {
+//   await User.findByIdAndUpdate(id, { age });
+//   return User.countDocuments({ age });
+// };
+
+// updateAgeAndCount('60fdfd7718dbae24a82f7d15', 2)
+// .then(count => console.log('count: ' + count))
+// .catch(error => console.log(error));
+
+// Task.findByIdAndDelete('61024a2ba197b608f4455fd0').then(task => {
+//   console.log(task);
+//   return Task.countDocuments({ completed: false }); 
+// }).then(count => {
+//     console.log(count);
+// }).catch(error => {
+//     console.log(error);
+// })
+
+deleteTaskAndCount = async (id) => {
+  await Task.findByIdAndDelete(id);
+  return Task.count({completed: false});
+};
+
+deleteTaskAndCount('610249a3a197b608f4455fcc')
+.then(count => console.log('count: ' + count))
+.catch(error => console.log(error));
